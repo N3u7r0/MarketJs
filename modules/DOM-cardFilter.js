a1c75a010c90783ee756a1c44321488508585a73
brian pc joa <brian_sabatini1993@hotmail.com>
2023-12-27 22:56:21 -0300

    ultimo commit

--- modules/DOM-cardFilter.js
+++ modules/DOM-cardFilter.js
@@ -3,21 +3,21 @@ import { productos } from "../../modules/products.js";
 export function cardFilter() {
     let divGlobal = document.getElementsByClassName("divGlobal")[0];
     const productosFiltrados = productos.filter(x => x.tipo == "notebook");
-    for (let c of productosFiltrados) {
+    for (let item of productosFiltrados) {
         let card = document.createElement("div");
         card.innerHTML =
             `
              <div  id="carta" class="card text-white bg-dark mb-3" style="max-width: 16rem;">
-                <img class="card-img-top" src="${c.foto}" alt="foto">
+                <img class="card-img-top" src="${item.foto}" alt="foto">
                 <div class="card-header">
-                    <h7 class="card-title"><strong>${c.marca}</strong></h7>
-                    <h7 class="card-title">${c.tipo}</h7>
+                    <h7 class="card-title"><strong>${item.marca}</strong></h7>
+                    <h7 class="card-title">${item.tipo}</h7>
                 </div>
                 <div class="card-body">
-                    <h6 class="card-title">${c.modelo}</h6>
-                    <p class="card-text">${c.descripcion}.</p>
-                    <p>stock: (${c.cantidad})</p>
-                    <p class="card-text"><strong>Precio: $${c.precio}</strong></p>
+                    <h6 class="card-title">${item.modelo}</h6>
+                    <p class="card-text">${item.descripcion}.</p>
+                    <p>stock: (${item.cantidad})</p>
+                    <p class="card-text"><strong>Precio: $${item.precio}</strong></p>
                     <hr>
                 <div>
                 <div class="d-grid gap-2">
